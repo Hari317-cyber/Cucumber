990c3378a2e4a998e189d1ddcbbf6858888e1505
Hari317-cyber <harithactajith@gmail.com>
2025-05-25 16:08:50 -0500

    pom and runner change 2

--- src/test/java/Runners/RunnerAll.java
+++ src/test/java/Runners/RunnerAll.java
@@ -8,6 +8,7 @@ import io.cucumber.testng.CucumberOptions;
         tags="@Login1 or @Login2 or @Search2 or @Search1",
         glue = {"StepDefinitions"},
         plugin={"pretty","html:TestReports\\TestReports.html"}
+        //
 )
 public class RunnerAll extends AbstractTestNGCucumberTests {
 }
