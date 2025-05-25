5e672c1fe9a882349b910e9689b1fe0f23dd32e3
Hari317-cyber <harithactajith@gmail.com>
2025-05-25 16:03:41 -0500

    pom and runner change

---
+++ src/test/java/Runners/RunnerAll.java
@@ -0,0 +1,13 @@
+package Runners;
+
+import io.cucumber.testng.AbstractTestNGCucumberTests;
+import io.cucumber.testng.CucumberOptions;
+
+@CucumberOptions(
+        features = "src/test/resources",
+        tags="@Login1 or @Login2 or @Search2 or @Search1",
+        glue = {"StepDefinitions"},
+        plugin={"pretty","html:TestReports\\TestReports.html"}
+)
+public class RunnerAll extends AbstractTestNGCucumberTests {
+}
